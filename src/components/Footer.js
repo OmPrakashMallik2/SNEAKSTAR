8584c3c55c12e80322e6370e7b999872ae230a54
Om Prakash Mallik <omprakashmallik91@gmail.com>
2023-08-02 23:32:59 +0530

    working on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,68 @@
-import React from 'react'
+import React from 'react';
+import "./Footer.css";
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import PaymentsIcon from '@mui/icons-material/Payments';
+import SecurityIcon from '@mui/icons-material/Security';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 function Footer() {
   return (
-    <div>
-      <h1>footer</h1>
+    <div className='Footer'>
+      <div className='l'>
+        <div>
+          <h1>MY SHOES</h1>
+        </div>
+
+        <div>
+          <ul>
+            <li><InstagramIcon/></li>
+            <li><FacebookIcon/></li>
+            <li><TwitterIcon/></li>
+          </ul>
+        </div>
+
+        <div>
+          <ul>
+            <li><a>Link1</a></li>
+            <li><a>Link1</a></li>
+            <li><a>Link1</a></li>
+            <li><a>Link1</a></li>
+            <li><a>Link1</a></li>
+            <li><a>Link1</a></li>
+            <li><a>Link1</a></li>
+            <li><a>Link1</a></li>
+          </ul>
+        </div>
+
+
+      </div>
+
+
+      <div className='r'>
+        <ul>
+          <li>
+            <LocalShippingIcon />
+            Free LocalShipping
+          </li>
+          <li>
+            <AccountBalanceWalletIcon />
+            Buy now pay latter
+          </li>
+          <li>
+            <PaymentsIcon />
+            Cash on delivery
+          </li>
+          <li>
+            <SecurityIcon />
+            100% secure Payments
+          </li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
