974cb4dcb38123c5dfb7afee3b4398f42b02fb7b
Om Prakash Mallik <omprakashmallik91@gmail.com>
2023-08-16 01:40:29 +0530

    Products completed

--- src/components/Product1.js
+++ src/components/Product1.js
@@ -8,14 +8,17 @@ function Product1() {
       <div className='pr1-info'>
         <h2>WHITE</h2>
         <p>If you're concerned about both fashion and comfort during sports and workouts, opt for this pair of designer running shoes to add some chutzpah to your look. This lace-up running shoe is stable enough on the foot.</p>
-        <p className='price'>&#x20B9;1999.00  <span>&#x20B9;599.00</span></p>
-        <Link to="pr1" className='btn'>Learn More </Link>
-        <Link to="buynow" className='btn'>Buy Now</Link>
+        <p className='price'>&#x20B9;1499.00  <span>&#x20B9;2499.00</span></p>
+        <div className='btn-cont'>
+          <Link to="pr1" className='btn'>Learn More </Link>
+          <Link to="buynow" className='btn'>Buy Now</Link>
+
+        </div>
       </div>
       <div className='pr1-pic'>
         {/* <img src='https://i.pinimg.com/564x/2f/e0/25/2fe025e8dd0b3445b98a4916fb40efc8.jpg'  alt='ky yar'/> */}
-        <img src={require("../Media/shoes1.jpg")}  alt='ky yar'/>
-        
+        <img src={require("../Media/shoes1.jpg")} alt='ky yar' />
+
       </div>
     </div>
   )
--- src/components/Product2.js
+++ src/components/Product2.js
@@ -8,15 +8,20 @@ function Product2() {
 
       <div className='pr2-pic'>
         {/* <img src='https://i.pinimg.com/750x/16/76/7b/16767bce07f7c53e651dff1fcac537da.jpg'  alt='ky yar'/> */}
-        <img src={require("../Media/shoes2.jpg")}  alt='ky yar'/>
+        <img src={require("../Media/shoes2.jpg")} alt='ky yar' />
       </div>
 
       <div className='pr2-info'>
         <h2>BLACK</h2>
-        <p>If you're concerned about both fashion and comfort during sports and workouts, opt for this pair of designer running shoes to add some chutzpah to your look. This lace-up running shoe is stable enough on the foot. The specialty of this running shoe is its versatility, breathability and cushioning for safety. Perfect for beginners! Specifications Brand Name: GeraldBlack Gender: MEN Origin: CN(Origin) Sports Type: LIFESTYLE Function: Cushioning Applicable Place: Outdoor Lawn Level Of Practice: Be</p>
-        <p className='price'>1999.00</p>
-        <Link to="pr2" className='btn'>learn More</Link>
-        <Link to="buynow" className='btn'>BUY NOW</Link>
+        <p>If you're concerned about both fashion and comfort during sport
+          s and workouts, opt for this pair of designer running shoes to add some chu
+          tzpah to your look. This lace-up running shoe is stable enough on the foot.</p>       
+        <p className='price'>&#x20B9;1999.00  <span>&#x20B9;2999.00</span></p>
+        <div className='btn-cont'>
+          <Link to="pr2" className='btn'>Learn More</Link>
+          <Link to="buynow" className='btn'>Buy Now</Link>
+
+        </div>
       </div>
 
     </div>
